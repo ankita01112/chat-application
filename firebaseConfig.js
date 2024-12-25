d31b92ff446742ecfced2f29a464675de856dd8e
ankita0112 <ankita006jha@gmail.com>
2024-12-25 21:27:03 +0530

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "https://worldchatapp-devxprite-default-rtdb.firebaseio.com",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
